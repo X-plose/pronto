6a2235a93b8aab9fa6d9c6baded51bb0ab5c94f1
Jeroen Jacobs <git@jeroenj.be>
2017-04-24 09:28:54 +0200

    Adds a --config option to pass a path to a .pronto.yml config file

--- lib/pronto/config_file.rb
+++ lib/pronto/config_file.rb
@@ -1,6 +1,7 @@
 module Pronto
   class ConfigFile
     DEFAULT_MESSAGE_FORMAT = '%{msg}'.freeze
+    DEFAULT_FILE_PATH = '.pronto.yml'.freeze
     DEFAULT_WARNINGS_PER_REVIEW = 30
 
     EMPTY = {
@@ -39,8 +40,12 @@ module Pronto
       'format' => DEFAULT_MESSAGE_FORMAT
     }.freeze
 
-    def initialize(path = '.pronto.yml')
-      @path = path
+    def initialize
+      @path = if File.exist?(ENV['PRONTO_CONFIG'].to_s)
+                ENV['PRONTO_CONFIG']
+              else
+                DEFAULT_FILE_PATH
+              end
     end
 
     def to_h
--- spec/pronto/config_file_spec.rb
+++ spec/pronto/config_file_spec.rb
@@ -34,6 +34,9 @@ module Pronto
       context 'only global excludes in file' do
         before do
           File.should_receive(:exist?)
+            .and_return(false)
+
+          File.should_receive(:exist?)
             .and_return(true)
 
           YAML.should_receive(:load_file)
@@ -49,8 +52,39 @@ module Pronto
         end
       end
 
+      context 'custom config file' do
+        let(:path) { '/tmp/pronto.yml' }
+
+        before do
+          ENV.should_receive(:[])
+            .with('PRONTO_CONFIG').twice
+            .and_return(path)
+
+          File.should_receive(:exist?)
+            .with(path)
+            .and_return(true)
+
+          File.should_receive(:exist?)
+            .and_return(true)
+
+          YAML.should_receive(:load_file)
+            .with(path)
+            .and_return('all' => { 'exclude' => ['a/**/*.rb'] })
+        end
+
+        it do
+          should include(
+            'all' => {
+              'exclude' => ['a/**/*.rb'], 'include' => []
+            }
+          )
+        end
+      end
+
       context 'a value is set to false' do
         before do
+          File.should_receive(:exist?)
+            .and_return(false)
           File.should_receive(:exist?)
             .and_return(true)
 
